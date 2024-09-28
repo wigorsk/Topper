210f12dc4fbe56ecc8d6c983bd1ca0792057d6eb
Wigor Skorek <wigorskorek@gmail.com>
2024-09-27 21:21:25 -0300

    changed basis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,32 +8,38 @@ import { use, useState } from "react";
 
 export default function Home() {
 
+  // className='basis-*'
+  const basis = 'basis-20 md:basis-28 lg:basis-44 '
+
+  // VARIÁVEIS
   const [popup, setPopup] = useState<boolean>(false);
 
   // Expandir alimento selecionado
   const [popupAddFood, setPopupAddFood] = useState<boolean>(false);
   const [food, setFood] = useState<number>(0)
 
-  
+  // Campo de Search
   const [search, setSearch] = useState<string>('');
   const searchFiltered = API.filter((item) => item.name.includes(search))
 
+  // Gasto Diário
   const tmb:number = 2100;
   const [dailyExpense, setDailyExpense] = useState<number>(1000)
   const remainingKcal:number = tmb - dailyExpense
 
-  const [grams, setGrams] = useState<string>('')
-
+  // Abrir Pop-up para pesquisar alimentos
   const handleButtonClick = () => {
     setPopup(!popup);
     setSearch('');
   }
 
+  // Abrir Pop-up para adicionar alimento
   const addFoodButton = (index: number) => {
     setPopupAddFood(!popupAddFood)
     setFood(index)
   }
 
+  // Adicionar alimento
   const AddFoodList = () => {
     setPopupAddFood(false)
     setPopup(false);
@@ -43,57 +49,76 @@ export default function Home() {
   return (
     <div className="w-screen h-screen bg-neutral-100">
       
-      <Header />
+      <Header /> {/* Menu */}
 
       <div className="container mx-auto flex flex-col items-center px-10 gap-5">
 
+        {/* Dia */}
         <div className="flex items-center gap-4 mt-5"> 
           <button><ChevronLeftIcon className=" size-10 "/></button>
           <h1 className="text-2xl font-bold">Hoje</h1>  
           <button><ChevronRightIcon className=" size-10 "/></button>
         </div>
 
-        <hr className="w-screen"/>
+        <hr className="w-screen"/> {/* Linha Horizontal */}
 
+        {/* Gasto Diário */}
         <div className="text-2xl font-bold flex items-center gap-5">
           <p>{tmb}</p> - <p>{dailyExpense}</p> = <p>{remainingKcal}</p>
         </div>
 
-        <div className="w-10/12 mt-10">
-          <div className="flex px-5 text-center font-semibold">
+        {/* Refeições */}
+        <div className="w-full xl:w-10/12 mt-10">
+
+          {/* Menu da Tabela */}
+          <div className="flex text-center text-xs md:text-sm font-semibold mb-5 sm:mb-0">
             <h1 className="flex-1"></h1>
-            <h1 className="basis-40">quantidade</h1>
-            <h1 className="basis-40">carboidratos</h1>
-            <h1 className="basis-40">gorduras</h1>
-            <h1 className="basis-40">proteínas</h1>
+            <h1 className={`${basis}`}>quantidade</h1>
+            <h1 className={`${basis}`}>carboidratos</h1>
+            <h1 className={`${basis}`}>gorduras</h1>
+            <h1 className={`${basis}`}>proteínas</h1>
           </div>
 
-          <div className="flex flex-col gap-4">
-            <table className="flex flex-col px-5">
-              <thead>
-                <tr className="flex"><th className="flex-1 text-start font-bold text-2xl">Café da Manhã</th></tr>
-              </thead>
-              <tbody className="flex flex-col gap-2">
 
-                <tr className="py-2 flex items-center text-center bg-neutral-300 text-neutral-950">
-                  <td className="flex-1 text-start ml-10">Nome do alimento</td>
-                  <td className="basis-40">...</td>
-                  <td className="basis-40">...</td>
-                  <td className="basis-40">...</td>
-                  <td className="basis-40">...</td>
-                </tr>
+          {/* Table */}
+          <table className="flex flex-col">
+            
+            {/* Período da Refeição */}
+            <thead className="font-bold text-sm sm:text-lg md:text-xl xl:text-2xl">
+              <tr><th className="flex-1 text-start">Café da Manhã</th></tr>
+            </thead>
 
-                <tr className="flex items-center text-center bg-blue-950 text-neutral-100">
-                  <td className="flex-1 text-start"><button className="ml-5 px-5 py-2" onClick={handleButtonClick}>Adicionar alimento</button></td>
-                  <td className="basis-40">...</td>
-                  <td className="basis-40">...</td>
-                  <td className="basis-40">...</td>
-                  <td className="basis-40">...</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+            {/* Body */}
+            <tbody className="flex flex-col gap-2">
+
+              {/* Alimento Adicionado */}
+              <tr className="py-2 flex items-center text-center bg-neutral-300 text-neutral-950">
+                <td className="flex-1 text-start ml-5 md:ml-10 text-xs sm:text-sm md:text-base line-clamp-1">Nome do alimento</td>
+                <td className={`${basis}`}>...</td>
+                <td className={`${basis}`}>...</td>
+                <td className={`${basis}`}>...</td>
+                <td className={`${basis}`}>...</td>
+              </tr>
+
+              {/* Button de Adicionar Comida */}
+              <tr className="flex items-center text-center bg-blue-950 text-neutral-100">
+                <td className="flex-1 text-start">
+                  <button 
+                    onClick={handleButtonClick}
+                    className="px-5 md:px-10 py-2 text-xs sm:text-sm md:text-base" 
+                    >Adicionar</button>
+                </td>
+                <td className={`${basis}`}>...</td>
+                <td className={`${basis}`}>...</td>
+                <td className={`${basis}`}>...</td>
+                <td className={`${basis}`}>...</td>
+              </tr>
+              
+            </tbody>
+
+          </table>
+
+        </div> 
 
         {popup && 
           <div className="absolute top-0 left-0 w-screen h-screen bg-black/50 flex items-center justify-center">
@@ -190,6 +215,7 @@ export default function Home() {
             </div>
           </div>
         }
+
       </div>
     </div>
   );
